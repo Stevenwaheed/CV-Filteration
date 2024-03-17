47dd5a602180d2f22b68f697fa5cf6ef7c37478c
Steven W. Saad <83607748+Stevenwaheed@users.noreply.github.com>
2024-03-17 22:15:59 +0200

    Update streamlit_2.py

--- streamlit_2.py
+++ streamlit_2.py
@@ -28,8 +28,8 @@ import os
 
 # txt2speech = TextToSpeech()
 
-print(st.secrets['API_KEY'])
-st.write(st.secrets['API_KEY'])
+print(st.secrets.get['API_KEY'])
+st.write(st.secrets.get['API_KEY'])
 client = OpenAI(api_key=st.secrets['API_KEY'])
 
 nltk.download('stopwords')
